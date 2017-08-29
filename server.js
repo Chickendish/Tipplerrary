d1ac3270cfb571bade3c4c07b62c62a2c0793f27
Bill Donovan <williamcdonovan@gmail.com>
2017-08-28 18:08:55 -0600

    added bootstrap template

--- server.js
+++ server.js
@@ -5,3 +5,21 @@
 var express = require('express');
 var exphbs	= require('express-handlebars');
 var app = express();
+
+var PORT = 5000;
+
+//======================================
+// 			Routes
+//======================================
+
+app.get('/', function(req,res){
+	res.send("hello world");
+});
+
+//======================================
+//			Start the server
+//======================================
+
+app.listen(PORT, function(err){
+	console.log("App listening on port:" + PORT);
+});
