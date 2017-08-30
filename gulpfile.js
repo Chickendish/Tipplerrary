f75d9af1387dbb0467cd9eb2aa5671364054f491
Bill Donovan <williamcdonovan@gmail.com>
2017-08-30 11:26:03 -0600

    added wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,16 @@ gulp.task('style', function(){
 		.pipe(jshint())
 		.pipe(fixmyjs());
 });
+
+gulp.task('inject',function(){
+	var wiredep = require('wiredep').stream;
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib'
+	}
+
+	return gulp.src('.src/views/*.html')
+	.pipe(wiredep(options))
+	.pipe(gulp.dest('.src/views'));
+
+})
