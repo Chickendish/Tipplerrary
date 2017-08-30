738fc8b5ff3a68dd9e631efd6f9d62df27cf1035
Bill Donovan <williamcdonovan@gmail.com>
2017-08-30 11:03:56 -0600

    added jshint and fixmyjs to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var fixmyjs = require("gulp-fixmyjs");
+
+var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function(){
-	gulp.src();
+	return gulp.src(jsFiles)
+		.pipe(jshint())
+		.pipe(fixmyjs());
 });
